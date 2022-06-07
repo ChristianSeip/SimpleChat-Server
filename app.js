5a36cef408b23573d1ef75cfd2ea2e52738e64f2
Christian Seip <christian.seip@ravitar.com>
2022-06-07 17:59:38 +0200

    Added Clientlist, SendAll Method and Session Validator.

--- app.js
+++ app.js
@@ -6,23 +6,41 @@ const wss = new WebSocket.Server({server})
 const Database = require('./lib/db/database');
 const User = require('./lib/user/user');
 require('dotenv').config();
-let clients = [];
+let channel = {
+    name: 'Welcome',
+    users: [],
+};
+
+process.on('uncaughtException', (error, origin) => {
+    console.log('----- Uncaught exception -----')
+    console.log(error)
+    console.log('----- Exception origin -----')
+    console.log(origin)
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('----- Unhandled Rejection at -----')
+    console.log(promise)
+    console.log('----- Reason -----')
+    console.log(reason)
+});
 
 // Initialize db connection and start server
 Database.connectToServer(async function () {
     server.listen(process.env.APP_PORT);
     console.log(`Listening on port ${process.env.APP_PORT}`);
-
 });
 
 wss.on('connection', function connection(ws) {
-    //clients.push(ws); // TODO: push client on login {uuid: {User(), ws, channel_id}}
     ws.on('message', function incoming(data) {
+
         try {
             data = JSON.parse(data.toString());
             if(!('event' in data)) {
                 return;
             }
+
+            let user = new User();
 
             switch (data.event) {
                 case 'NewUser':
@@ -31,7 +49,6 @@ wss.on('connection', function connection(ws) {
                     });
                     break;
                 case 'Login':
-                    let user = new User();
                     user.login(data.data).then((response) => {
                         ws.send(JSON.stringify(response));
                     });
@@ -43,28 +60,35 @@ wss.on('connection', function connection(ws) {
                     break;
                 case 'GetProfile':
                     break;
-                case 'UpdateProfile':
-                    break;
                 case 'SendMessage':
-                    // Unterscheidung: Private, Public, Broadcaste > Modul entscheiden
-                    break;
-                case 'GetChannelList':
                     break;
                 case 'JoinChannel':
+                        user.getUserById(data.data.uuid).then(() => {
+                            user.isValidSession(data.data.sid).then((valid) => {
+                                console.log(valid)
+                                if(valid) {
+                                    user.setActivity();
+                                    channel.users[user.getId] = {socket: ws, client: user}
+                                    wss.sendAll(`${user.getUsername} has joined.`);
+                                }
+                                else {
+                                    ws.terminate()
+                                }
+                            });
+                        });
                     break;
-            }
+                }
         }
         catch (e) {
             console.log(`Invalid incoming data: ${data}, Error: ${e}`);
         }
-
-        /*
-        //Send data to all Clients
-        wss.clients.forEach(function each(client) {
-           if (client.readyState === WebSocket.OPEN) {
-               client.send(data.toString());
-           }
-        });
-         */
     });
 });
+
+wss.sendAll = function (msg) {
+    Object.keys(channel.users).forEach((key) => {
+        if(channel.users[key].socket.readyState === WebSocket.OPEN) {
+            channel.users[key].socket.send(JSON.stringify({event: 'PublicSystemMessage', data: {msg: msg}}));
+        }
+    });
+}
